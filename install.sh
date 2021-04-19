429d35a0eb4ec6a6799fd61aefc03f59f20058c4
Garik <mail@garik.sitee>
2021-04-19 19:22:16 +0400

    deploy

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-docker-compose up --build -d
-docker exec -ti solana bash install.sh
-docker exec -ti solana /bin/sh -c 'cd build/solana-release && export PATH=$PWD/bin:$PATH && cd .. && cd .. && solana --version'
-mv install.sh install.sh.off
-docker exec -ti solana bash
