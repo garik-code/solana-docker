88234689713f4aedf701afb015430197a7543386
Igor M <mail@garik.site>
2021-04-23 01:08:28 +0400

    Update terminal.sh

--- terminal.sh
+++ terminal.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 clear
-docker exec -ti solana /bin/sh -c 'cd build/solana-release && export PATH=$PWD/bin:$PATH && cd .. && cd .. && sudo solana-sys-tuner --user $(whoami) > sys-tuner.log 2>&1 & solana --version && bash'
+docker exec -ti solana /bin/sh -c 'cd build/solana-release && export PATH=$PWD/bin:$PATH && cd .. && cd .. && solana --version && bash'
